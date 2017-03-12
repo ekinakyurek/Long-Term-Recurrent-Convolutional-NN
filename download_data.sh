b47ae1241a17cd29ba63c6fb611e1bd6d0b860cd
ekinakyurek <akyurekekin@gmail.com>
2017-03-12 16:11:07 +0000

    Assignments

--- download_data.sh
+++ download_data.sh
@@ -12,8 +12,13 @@ cd ../
 mkdir MsCoCo
 cd MsCoCo
 mkdir train_images
+mkdir val_images
 cd ../
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 unzip train2014.zip -d MsCoCo/train_images
-
-
+rm train2014.zip
+wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+unzip val2014.zip -d MsCoCo/val_images
+rm val2014.zip
+wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip
+unzip captions_train-val2014.zip -d MsCoCo/
