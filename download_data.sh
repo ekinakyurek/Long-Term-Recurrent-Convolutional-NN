c407ff7b055a8da9796e90b9a0450c4e8bdfacf0
ekinakyurek <akyurekekin@gmail.com>
2017-03-13 21:35:55 +0300

    download_data.sh is updated

--- download_data.sh
+++ download_data.sh
@@ -2,23 +2,22 @@ echo "Downloading Flickr30k images and tokenized captions"
 mkdir Flickr30k
 cd  Flickr30k
 wget http://shannon.cs.illinois.edu/DenotationGraph/data/flickr30k-images.tar
-tar -xzvf flickr30k-images.tar
+tar -xf flickr30k-images.tar
 rm flickr30k-images.tar
 wget http://shannon.cs.illinois.edu/DenotationGraph/data/flickr30k.tar.gz
-tar -xzvf flickr30k.tar.gz
+tar -xzf flickr30k.tar.gz
 rm flickr30k.tar.gz
 echo "Succesfully downloaded in to Flickr30k folder"
 cd ../
 mkdir MsCoCo
 cd MsCoCo
-mkdir train_images
-mkdir val_images
 cd ../
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-unzip train2014.zip -d MsCoCo/train_images
+unzip -qq train2014.zip -d MsCoCo/
 rm train2014.zip
 wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
-unzip val2014.zip -d MsCoCo/val_images
+unzip -qq val2014.zip -d MsCoCo/
 rm val2014.zip
 wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip
-unzip captions_train-val2014.zip -d MsCoCo/
+unzip -qq captions_train-val2014.zip -d MsCoCo/
+rm captions_train-val2014.zip
