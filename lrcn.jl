364dc7f1088f8a0a59cb58c57c19161643dd5db0
ekinakyurek <akyurekekin@gmail.com>
2017-03-26 14:40:37 +0300

    minibatching is completed

--- lrcn.jl
+++ lrcn.jl
@@ -4,6 +4,8 @@ end
 using Knet
 
 !isdefined(:VGG) && include(Knet.dir("examples","vgg.jl"))
+!isdefined(:Tokenizer) && include("./tokenizer.jl")
+
 
 """
 julia lrcn.jl image-file-or-url
@@ -18,12 +20,14 @@ Proceedings of the IEEE conference on computer vision and pattern recognition. 2
 """
 module LRCN
 
-using Knet, AutoGrad, ArgParse,Compat, MAT, Images
+using Knet, AutoGrad, ArgParse,Compat, MAT, Images;
 
-using Main.VGG: data, imgurl
+#using Main.VGG: data, imgurl;
+
+using Tokenizer;
 
 const imgurl = "https://github.com/BVLC/caffe/raw/master/examples/images/cat.jpg"
-const default_file = "data/Flickr30k/results_20130124.token"
+const Flickr30k_captions = "data/Flickr30k/results_20130124.token"
 const eos = "~~"
 function main(args=ARGS)
     s = ArgParseSettings()
@@ -40,7 +44,7 @@ function main(args=ARGS)
         ("--hidden"; nargs='+'; arg_type=Int; default=[1000]; help="Sizes of one or more LSTM layers.")
         ("--embed"; arg_type=Int; default=1000; help="Size of the embedding vector.")
         ("--epochs"; arg_type=Int; default=20; help="Number of epochs for training.")
-        ("--batchsize"; arg_type=Int; default=1; help="Number of sequences to train on in parallel.")
+        ("--batchsize"; arg_type=Int; default=20; help="Number of sequences to train on in parallel.")
         ("--seqlength"; arg_type=Int; default=100; help="Number of steps to unroll the network for.")
         ("--decay"; arg_type=Float64; default=0.9; help="Learning rate decay.")
         ("--lr"; arg_type=Float64; default=4.0; help="Initial learning rate.")
@@ -68,8 +72,8 @@ function main(args=ARGS)
 
     # we initialize a model from loadfile, train using datafiles (both optional).
     # if the user specifies neither, train a model using the charlm.jl source code.
-    isempty(o[:datafiles]) && o[:loadfile]==nothing && push!(o[:datafiles],default_file) # Flickr30k
-    include("tokenizer.jl")
+    isempty(o[:datafiles]) && o[:loadfile]==nothing && push!(o[:datafiles],Flickr30k_captions) # Flickr30k
+
     # read text and create global vocab variable
 
     #text = map((@compat readstring), o[:datafiles])
@@ -81,10 +85,23 @@ function main(args=ARGS)
     # vocab (char_to_index) comes from the initial model if there is one, otherwise from the datafiles.
     # if there is an initial model make sure the data has no new vocab
     if o[:loadfile]==nothing
-        vocab, caption_dicts = Tokenizer.tokenize(data_files=o[:datafiles])
+
+        println("Tokenization starts")
+        #vocab = {"words1"=>1,"words2"=>2, ....}
+        vocab = Dict{String, Int}()
+        #captions_dicts = [((id1, ["word1","word2",...]),length1),((id2, ["word1","word2",...]),length2),...]
+        caption_dicts = Array{Array{Tuple{Tuple{Int64,Array{String,1}},Int64},1},1}();
+        Tokenizer.tokenize(vocab, caption_dicts;data_files=o[:datafiles])
         get!(vocab,eos,1+length(vocab)); #eos
-        w = cnn_weights(o[:embed]);
+        info("Tokenization finished with captions size: $(sizeof(caption_dicts))")
+
+        println("Intializing cnn_weights")
+        w = cnn_weights(o[:embed];atype=o[:atype]);
+        info("Cnn is initialized with size : $(sizeof(w))")
+
+        println("Intializing LSTM weigths")
         model = initweights(o[:atype], o[:hidden], length(vocab), o[:embed], o[:winit])
+        info("LSTM is initialized with size : $(sizeof(model))")
     else
         # info("Loading model from $(o[:loadfile])")
         # vocab = load(o[:loadfile], "vocab")
@@ -92,18 +109,20 @@ function main(args=ARGS)
         # model = map(p->convert(o[:atype],p), load(o[:loadfile], "model"))
     end
 
-    info("$(length(vocab)) unique words.")
+    info("$(length(vocab)) unique words with size: $(sizeof(vocab))")
 
 
-  #  info("Reading $(o[:image])")
+    #  info("Reading $(o[:image])")
     #img = data(o[:image], zeros(Float32,224,224,3,1))
 
 
     if o[:generate] > 0
+        println("Generation starts")
         state = initstate(o[:atype],o[:hidden],1)
         img = data(o[:image], zeros(Float32,224,224,3,1))
         #image = ones(224,224,3,1);
         generate(model, w, state, img, vocab, o[:generate])
+        println("Generation finished")
     end
 
     if !isempty(caption_dicts)
@@ -113,7 +132,11 @@ function main(args=ARGS)
       # p1 = exp(logp(z1))
       # display(hcat(p1[s1[1:o[:top]]], description[s1[1:o[:top]]]))
       # println()
-      train!(model,w, caption_dicts, vocab, o)
+      sequence = map(t->minibatch(t, vocab, o[:batchsize]), caption_dicts)
+      #no need to caption dicts any more delete it from memory
+      caption_dicts = 0; gc();
+      println("Data is created with size: $(sizeof(sequence))")
+      train!(model,w, sequence, vocab, o)
     end
 
     if o[:savefile] != nothing
@@ -123,26 +146,25 @@ function main(args=ARGS)
 
 end
 
-function train!(model, w, caption_dicts, vocab, o)
+function train!(model, w, sequence, vocab, o)
     s0 = initstate(o[:atype], o[:hidden], o[:batchsize])
-    data = map(t->minibatch(t, vocab, o[:batchsize]), caption_dicts)
     lr = o[:lr]
     if o[:fast]
         @time (for epoch=1:o[:epochs]
-               train1(model,w, copy(s0), data[1]; slen=o[:seqlength], lr=lr, gclip=o[:gclip])
+               train1(model,w, copy(s0), sequence[1]; batch_size=o[:batchsize], lr=lr, gclip=o[:gclip])
                end; gpu()>=0 && Knet.cudaDeviceSynchronize())
         return
     end
-    losses = map(d->report_loss(model, w, copy(s0),d), data)
+    losses = map(d->report_loss(model, w, copy(s0),d, o[:batchsize]), sequence)
     println((:epoch,0,:loss,losses...))
     devset = ifelse(length(data) > 1, 2, 1)
     devlast = devbest = losses[devset]
     for epoch=1:o[:epochs]
-        @time train1(model, copy(s0), data[1]; slen=o[:seqlength], lr=lr, gclip=o[:gclip])
+        @time train1(model, copy(s0), sequence[1]; slen=o[:seqlength], lr=lr, gclip=o[:gclip])
         @time losses = map(d->report_loss(model, w,copy(s0),d), data)
         println((:epoch,epoch,:loss,losses...))
         if o[:gcheck] > 0
-            gradcheck(loss, model, copy(s0), data[1], 1:o[:seqlength]; gcheck=o[:gcheck])
+            gradcheck(loss, model, copy(s0), sequence[1], 1:o[:seqlength]; gcheck=o[:gcheck])
         end
         devloss = losses[devset]
         if devloss < devbest
@@ -160,71 +182,136 @@ function train!(model, w, caption_dicts, vocab, o)
     end
 end
 
-function get_word_count(caption_dict)
-  count = 0;
-  for (k,v) in caption_dict
-    for i=1:5
-      for word in v[i]
-          if length(word) > 0
-            count += 1;
-          end
-      end
-      #eos
-      count += 1;
+function delete_unbatchable_captions!(caption_dict,batch_size)
+    lengths = map(t->t[2], caption_dict)
+
+    limit = length(lengths)-batch_size+1;
+    max_length =  maximum(lengths)
+
+    current_length = lengths[1]
+
+    current_index = 1;
+
+    ranges = Int64[];
+
+    while current_index < limit
+
+        if lengths[current_index+batch_size-1]==current_length
+            current_index += batch_size;
+        else
+            old_index = current_index;
+            current_index = 0;
+            while current_index==0
+                current_length+=1;
+                if current_length > max_length
+                  break
+                end
+                current_index = findfirst(lengths,current_length)
+            end
+             append!(ranges,collect(old_index:current_index-1))
+        end
+
+        if current_index > limit
+            append!(ranges,collect(current_index:length(lengths)))
+            break;
+        end
+
     end
-  end
-  return count;
+    deleteat!(caption_dict,ranges);
+    return caption_dict;
 end
 
 
-
 function minibatch(caption_dict, word_to_index, batch_size)
-    println("MinitBatching Starts")
-    nbatch = div(get_word_count(caption_dict), batch_size)
-    data = [falses(1, length(word_to_index)) for i=1:nbatch ]
-    ranges = Array{UnitRange}(length(caption_dict)*5);
-    inputs = Array{Int}(length(caption_dict)*5);
+    println("Minibatching starts")
 
-    id = 1
-    caption = 1;
-    for (k,v) in caption_dict
-      for i=1:5
-        #data size for one caption  1+length(v[i]) (+1 for eos)
-        ranges[caption] = caption > 1 ? (last(ranges[caption-1])+1:1+length(v[i])+last(ranges[caption-1])):(1:1+length(v[i]))
-        inputs[caption] = k;
-        caption += 1;
-        for word in v[i]
-          if length(word) > 0
-            col = word_to_index[word]
-            data[id][col] = 1
-            id += 1;
+    #to minibatch the data
+    println("Deleting unbatchable captions: $(length(caption_dict))")
+    delete_unbatchable_captions!(caption_dict, batch_size)
+    println("Unbatchable captions is deleted: $(length(caption_dict))")
+
+    #length for one caption  1+caption[i][2] because of eos
+    lengths = map(t->(t[2]+1), caption_dict)
+    #total word count
+    nbatch = div(sum(lengths), batch_size)
+
+    #initialize output sequence, input sequence, and range of the LSTM network for each batch
+    sequence = [falses(batch_size, length(word_to_index)) for i=1:nbatch];
+
+    input_ids = [ones(Int,batch_size) for i=1:batch_size:length(lengths)]
+
+    index = 1; l = 1; input_index = 1;
+
+    for i=1:batch_size:length(lengths)
+        l = lengths[i]
+        for j=i:i+batch_size-1
+          (id,words), _ = caption_dict[j]
+          input_ids[input_index][j-i+1] = id
+          for k=index:index+l-2
+             sequence[k][j-i+1, word_to_index[words[k-index+1]]] = 1
           end
+          sequence[index+l-1][j-i+1,end] = 1
         end
-        #eos
-        data[id][end] = 1
-        id += 1;
-      end
+        index = index+l;
+        input_index += 1;
     end
-    println("MinitBatching Finished")
-    return data,inputs,ranges
+
+    println("Minibatching finished")
+    return sequence,input_ids,lengths
+end
+
+function read_image_data(img, averageImage)
+    if contains(img,"://")
+        info("Downloading $img")
+        img = download(img)
+    end
+    a0 = load(img)
+    new_size = ntuple(i->div(size(a0,i)*224,minimum(size(a0))),2)
+    a1 = Images.imresize(a0, new_size)
+    i1 = div(size(a1,1)-224,2)
+    j1 = div(size(a1,2)-224,2)
+    b1 = a1[i1+1:i1+224,j1+1:j1+224]
+    c1 = permutedims(channelview(b1), (3,2,1))
+    d1 = convert(Array{Float32}, c1)
+    e1 = reshape(d1[:,:,1:3], (224,224,3,1))
+    f1 = (255 * e1 .- averageImage)
+    g1 = permutedims(f1, [2,1,3,4])
+    x1 = KnetArray(g1)
 end
 
 # sequence[t]: input token at time t
 # state is modified in place
-function train1(param, w, state, data; slen=100, lr=1.0, gclip=0.0)
-   sequence = data[1]
-   inputs = data[2]
-   ranges = data[3]
-    for t = 1:length(ranges)
-        input_id = inputs[t]
+function train1(param, w, state, seq; batch_size=20, lr=1.0, gclip=0.0)
+   sequence = seq[1]
+   input_ids = seq[2]
+   lengths = seq[3]
+
+
+    index = 1; l=1; input_index = 1;
+
+    for t = 1:batch_size:length(lengths)
+        l = lengths[t]
+        #input_ids = inputs_ids[index:index+l-1]
         #println(input_id)
-        input = Main.VGG.data("./data/Flickr30k/flickr30k-images/$input_id.jpg", zeros(Float32,224,224,3,1))
-        #input = ones(Float32,224,224,3,1);
+
+        input = KnetArray(Float32,224,224,3,batch_size);
+
+        for i=1:batch_size
+              id = input_ids[input_index][i]
+              start_index = 1;
+              end_index = 150528;
+              if i != 1
+                  start_index = sub2ind((224,224,3,batch_size),224,224,3,i-1) + 1
+                  end_index = start_index + 150528 - 1;
+              end
+              input[start_index:end_index] = read_image_data("./data/Flickr30k/flickr30k-images/$id.jpg", zeros(Float32,224,224,3,1))
+        end
+
         input = cnn_predict(w, input)
-        #input = ones(Float32, 1, 1000);
-        @time gloss = lossgradient(param, state, input, sequence, ranges[t])
+        #inputs = ones(Float32, batch_size, 1000);
+        gloss = lossgradient(param, state, input, sequence, index:index+l-1)
         println(t,". sentence trained")
-        println("loss in sentence: ", loss(param, state, input, sequence, ranges[t]))
+        #println("loss in sentence: ", loss(param, state, inputs, sequence, ranges[t]))
         gscale = lr
         if gclip > 0
             gnorm = sqrt(mapreduce(sumabs2, +, 0, gloss))
@@ -241,8 +328,11 @@ function train1(param, w, state, data; slen=100, lr=1.0, gclip=0.0)
         for i = 1:length(state)
             state[i] = AutoGrad.getval(state[i])
         end
+        index = index +l-1;
+        input_index += 1
     end
 end
+
 
 
 function initweights(atype, hidden, vocab, embed, winit)
@@ -293,7 +383,7 @@ end
 
 
 
-function loss(param,state,input, sequence,range)
+function loss(param,state,input,sequence,range)
     total = 0.0; count = 0
     atype = typeof(AutoGrad.getval(param[1]))
     input = convert(atype,input)
@@ -338,28 +428,49 @@ function sample(p)
     end
 end
 
-function report_loss(param, w, state, data)
+function report_loss(param, w, state, seq, batch_size)
   total = 0.0; count = 0
   atype = typeof(AutoGrad.getval(param[1]))
-  sequence = data[1]
-  inputs = data[2]
-  ranges = data[3]
 
-  for i=1:length(ranges)
-    input_id = inputs[i]
-    input = Main.VGG.data("./data/Flickr30k/flickr30k-images/$input_id.jpg", zeros(Float32,224,224,3,1))
+  sequence = seq[1]
+  input_ids = seq[2]
+  lengths = seq[3]
+
+  index = 1; l=1; input_index = 1;
+
+  for t = 1:batch_size:length(lengths)
+    l = lengths[t]
+
+    input = KnetArray(Float32,224,224,3,batch_size);
+
+    for i=1:batch_size
+          id = input_ids[input_index][i]
+          start_index = 1;
+          end_index = 150528;
+          if i != 1
+              start_index = sub2ind((224,224,3,batch_size),224,224,3,i-1) + 1
+              end_index = start_index + 150528 - 1;
+          end
+          input[start_index:end_index] = read_image_data("./data/Flickr30k/flickr30k-images/$id.jpg", zeros(Float32,224,224,3,1))
+    end
+
     #input = ones(Float32,224,224,3,1);
     input = cnn_predict(w, input)
     #input = ones(Float32, 1, 1000);
 
-    for t in ranges[i]
+    for c in index:index+l-1
         ypred = lrcn(param,state,input)
         ynorm = logp(ypred,2) # ypred .- log(sum(exp(ypred),2))
-        ygold = convert(atype, sequence[t])
+        ygold = convert(atype, sequence[c])
         total += sum(ygold .* ynorm)
         count += size(ygold,1)
         #input = ygold
     end
+
+    println("after",t,". batch loss is equal :", -total/count)
+
+    index = index+l-1;
+    input_index += 1;
 
   end
   return -total / count
@@ -426,22 +537,25 @@ function cnn_loss(w,x,ygold)
     -sum(ygold .* ynorm) / size(ygold,2)
 end
 
-function cnn_predict(w,x)
-  x = pool(relu(conv4(w[1],x;padding=0) .+ w[2]); window=2, stride=2);
-  x = pool(tanh(conv4(w[3],x;padding=0) .+ w[4]); window=2, stride=2);
+function cnn_predict(w,x; embed = 1000)
+  x = pool(relu(conv4(w[1],x;padding=2) .+ w[2]); window=2, stride=2);
+  x = pool(relu(conv4(w[3],x;padding=2) .+ w[4]); window=2, stride=2);
+  x = pool(relu(conv4(w[5],x;padding=2) .+ w[6]); window=2, stride=2);
+  x = pool(relu(conv4(w[7],x;padding=2) .+ w[8]); window=2, stride=2);
   #println(size(x))
   #DBG Change reshape to make it row vector not column since it goes to LSTM eventually
-  x = reshape(x,(53*53*12,size(x,4)));
-  x = tanh(w[5]*x .+ w[6]);
-  x = reshape(x, (1,1000));
+  x = reshape(x,(size(x,4),14*14*10));
+  x = tanh(x*w[9] .+ w[10]);
   return x;
 end
 
 function cnn_weights(embed; atype=KnetArray{Float32}, winit=0.1)
-  w = Array(Any, 6)
-  w[1] = randn(Float32,5,5,3,12)*winit; w[2] = zeros(Float32,1,1,12,1);
-  w[3] = randn(Float32,5,5,12,12)*winit; w[4] = zeros(Float32,1,1,12,1);
-  w[5] = randn(Float32,embed,53*53*12)*winit; w[6] = zeros(Float32,embed,1);
+  w = Array(Any, 10)
+  w[1] = randn(Float32,5,5,3,10)*winit; w[2] = zeros(Float32,1,1,10,1);
+  w[3] = randn(Float32,5,5,10,10)*winit; w[4] = zeros(Float32,1,1,10,1);
+  w[5] = randn(Float32,5,5,10,10)*winit; w[6] = zeros(Float32,1,1,10,1);
+  w[7] = randn(Float32,5,5,10,10)*winit; w[8] = zeros(Float32,1,1,10,1);
+  w[9] = randn(Float32,14*14*10, embed)*winit; w[10] = zeros(Float32,1,embed);
   return map(a->convert(atype,a), w);
 end
 
--- tokenizer.jl
+++ tokenizer.jl
@@ -1,68 +1,76 @@
 module Tokenizer
 using JSON
-default_caption_files = ["data/Flickr30k/results_20130124.token", "data/MsCoco/captions_val2014.json", "data/MsCoco/captions_train2014.json"]
+
+#default_caption_files = ["data/Flickr30k/results_20130124.token", "data/MsCoco/captions_val2014.json", "data/MsCoco/captions_train2014.json"]
 default_caption_files = ["data/Flickr30k/results_20130124.token"];
-function tokenize(;data_files=default_caption_files)
-  global vocab = Dict{String, Int}()
-  global captions_dicts = Any[];
+function tokenize(vocab, caption_dicts; data_files=default_caption_files)
   for i=1:length(data_files)
       file = open(data_files[i])
       tokenization_type = split(data_files[i],'.')[2]
       if tokenization_type == "token"
-        captions_dict = tokenize_flicker_captions(readlines(file));
-        push!(captions_dicts, captions_dict)
-        vocab = get_vocab(captions_dict, vocab)
+        dict = tokenize_flicker_captions(readlines(file));
+        push!(caption_dicts, dict)
+        vocab = get_vocab(dict, vocab)
       elseif tokenization_type == "json"
-        captions_dict = tokenize_mscoco_captions(readlines(file));
-        push!(captions_dicts, captions_dict)
-        vocab = get_vocab(captions_dict, vocab)
+        dict = tokenize_mscoco_captions(readlines(file));
+        push!(caption_dicts, dict)
+        vocab = get_vocab(dict, vocab)
       else
         println("invalid caption file: ", data_files[i])
       end
       close(file)
   end
-  return vocab, captions_dicts ;
+  return vocab, caption_dicts ;
 end
 
 function tokenize_flicker_captions(captions)
-  captions_dict = Dict{Int,Array{Array{AbstractString,1},1}}()
+  captions_dict = Array{Tuple{Tuple{Int64,Array{String,1}},Int64},1}()
   for i=1:length(captions)
-      words = map(lowercase, split(captions[i],' '))
-      for i=2:length(words)-1
-        words[i] = lowercase(strip(words[i],['\"', '.', ',', '#', '&', '\'', ')', '(', '!', ' ', '/', '?', '\t']))
+      words = map(lowercase, split(captions[i],[' ','\t','#','.', '\n']))
+      id = words[1]
+      id = parse(Int64,id)
+      count = 4;
+      for i=4:length(words)
+        words[count] = lowercase(strip(words[count],[' ', '.', ',','#', '\'', ')', '(', '!', '/', '?', '\t']))
+        if length(words[count]) < 1
+          deleteat!(words,count)
+          count-=1;
+        end
+        count += 1;
       end
-      id = split(split(words[1],'#')[1],'.')[1];
-      id = parse(Int,id)
-      a = get!(captions_dict, id , [])
-      push!(a, words[2:end-1]);
+      push!(captions_dict, ((id,words[4:end]),count-4));
   end
+  captions_dict = sort(captions_dict, by = tuple -> last(tuple), rev=false)
   return captions_dict
 end
 
 function tokenize_mscoco_captions(captions)
-    captions_dict = Dict{Int,Array{Array{AbstractString,1},1}}()
+    captions_dict = Array{Tuple{Tuple{Int64,Array{String,1}},Int64},1}()
     data = JSON.parse(captions[1])["annotations"]
     for obj in data
       words = split(obj["caption"],' ');
+      count = 1;
       for i=1:length(words)
-        words[i] = lowercase(strip(words[i],['\"', '.', ',', '#', '&', '\'', ')', '(', '!', ' ', '/', '?', '\t']))
+        words[count] = lowercase(strip(words[count],[' ', '.', ',', '#', '\'', ')', '(', '!', '/', '?', '\t']))
+        if length(words[count]) < 1
+          deleteat!(words,count)
+          count-=1;
+        end
+        count += 1;
       end
       id = obj["image_id"]
-      a = get!(captions_dict, id ,[])
-      push!(a, words)
+      push!(captions_dict, ((id,words),count-1));
     end
+    captions_dict = sort(captions_dict, by = tuple -> last(tuple), rev=false)
     return captions_dict
 end
 
 function get_vocab(dict, vocab)
-  for (k,v) in dict
-    for word_arrays in v
-      for word in word_arrays
-          #word=strip(word,['\"', '.', ',', '#', '&', '\'', ')', '(', '!', ' ', '/', '?', '\t'])
+  for data in dict
+    for word in data[1][2]
           if length(word) > 0
             get!(vocab, word, 1+length(vocab));
           end
-      end
     end
   end
   return vocab
